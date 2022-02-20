195008988d881b11a8f2fa402ee3dca3eed8171c
Patryk <patrykprause@gmail.com>
2022-02-20 20:46:00 +0100

    done

--- announcements/views.py
+++ announcements/views.py
@@ -8,29 +8,29 @@ from .forms import AnnouncementForm, AvailabilityForm, CommentForm
 from userprofile.models import Userprofile
 # Create your views here.
 
-@login_required
+# @login_required
 def announcement_detail_view(request, id=None):
-    user = get_object_or_404(Userprofile, user=request.user)
     obj = get_object_or_404(Announcement, id=id)
     author = get_object_or_404(Userprofile, user = obj.user)
     form = CommentForm(request.POST or None)
     context = {
         'obj':obj,
         'form':form,
-        'user':user,
         'author':author
     }
-    if form.is_valid():
-        com = form.save(commit=False)
-        com.user = request.user
-        com.announcement = obj
-        com.save()
-        form = CommentForm()
-        context['form'] = form
-        return render(request, "form_comments.html", context)
+    if request.method == "POST":
+        user = get_object_or_404(Userprofile, user=request.user)
+        if form.is_valid():
+            com = form.save(commit=False)
+            com.user = request.user
+            com.announcement = obj
+            com.save()
+            form = CommentForm()
+            context['form'] = form
+            return render(request, "form_comments.html", context)
     return render(request, "announcements/detail.html", context)
 
-@login_required
+# @login_required
 def announcement_list_view(request):
     qs = reversed(Announcement.objects.all())
     context = {
@@ -126,17 +126,11 @@ def announcement_update_view(request, id=None):
             context['message'] = 'zapisano'
             form = AnnouncementForm(request.POST or None, instance = an)
             return render(request, "announcements/create-update.html", context)
-    # if request.method == "POST":
-    #     if form.is_valid():
-    #         an = form.save(commit=False)
-    #         an.save()
-    #         context['message'] = 'zapisano'
-    #     return redirect(an.get_hx_av_url())
+
     return render(request, "announcements/create-update.html", context)
 
 @login_required
 def announcement_delete_view(request, id=None, parent_id=None):
-
     try:
         obj = Announcement.objects.get(id=id, user = request.user)
     except:
