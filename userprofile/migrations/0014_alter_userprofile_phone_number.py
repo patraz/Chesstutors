03b56ae6958386a27850797651057b7ec7cec0c1
Patryk <patrykprause@gmail.com>
2022-02-21 20:34:22 +0100

    added phone number

--- userprofile/migrations/0014_alter_userprofile_phone_number.py
+++ userprofile/migrations/0014_alter_userprofile_phone_number.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('userprofile', '0013_userprofile_phone_number'),
+        ('userprofile', '0012_userprofile_profile_picture'),
     ]
 
     operations = [
