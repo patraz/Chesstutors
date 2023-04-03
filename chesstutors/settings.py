1bfe79aa3c4700323f5f8136555d5249c7716ec1
Patryk <patrykprause@gmail.com>
2023-04-03 20:37:58 +0200

    testing

--- chesstutors/settings.py
+++ chesstutors/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 
 from pathlib import Path
-import os
 import environ
 
 env = environ.Env()
@@ -22,7 +21,6 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DEBUG_PROPAGATE_EXCEPTIONS = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -44,7 +42,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'storages',
     'core',
     'django_htmx',
     'userprofile',
--- runserver.sh
+++ runserver.sh
@@ -1,4 +1,3 @@
-
 python manage.py makemigrations
 python manage.py migrate
 gunicorn chesstutors.wsgi --bind=0.0.0.0:80
